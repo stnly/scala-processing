60e31f80dc3f0d119e9a285137ae617006924095
Stanley Tan <me@stnly.com>
2013-04-15 11:51:52 +1000

    Clean up source

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,14 +14,16 @@ class Main extends PApplet {
   protected class RichRandom[K](seq: Seq[K]) {
     def random: K = seq((rand.nextFloat * seq.length).toInt)
   }
-  val d = 200
-  override def setup {
 
+  val d = 200
+
+  override def setup {
     size(d * 2, d * 2)
     background(0, 10, 20)
   }
-    var a = List(List(d / 2, 0, 255, 0, 0), List(d, d, 0, 255, 0), List(0, d, 0, 0, 255))
-    var p = List(d, 0, 255, 0, 0)
+
+  var a = List(List(d / 2, 0, 255, 0, 0), List(d, d, 0, 255, 0), List(0, d, 0, 0, 255))
+  var p = List(d, 0, 255, 0, 0)
 
   override def draw() {
     for(i <- 1 to 10) {
